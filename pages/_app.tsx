0310eab4a6b11deafe83849483582aec8f6d258d
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2022-01-31 06:50:32 -0800

    Add tina cms

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
+import { TinaEditProvider } from 'tinacms/dist/edit-state'
+const TinaCMS = dynamic(() => import('tinacms'), { ssr: false })
 import '../styles/index.css'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const branch = process.env.NEXT_PUBLIC_EDIT_BRANCH || "main";
+const apiURL =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:4001/graphql"
+    : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <TinaEditProvider
+        editMode={
+          <TinaCMS
+            apiURL={apiURL}
+            {...pageProps}
+          >
+            {(livePageProps) => <Component {...livePageProps} />}
+          </TinaCMS>
+        }
+      >
+        <Component {...pageProps} />
+      </TinaEditProvider>
+    </>
+  )
 }
+
+export default App
